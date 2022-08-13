648e3db75b7604de0d35bc5f4e97885d13519174
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-08-13 22:34:42 +0430

    feat: add todo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>hellow world</h1>
+  <input type="text" placeholder="what is your todo ?" v-model="name" />
+
+  <form @submit.prevent="addTodo">
+    <h4>What is on your todo list?</h4>
+    <input type="text" placeholder="todo list" v-model="input_content" />
+
+    <input type="submit" value="Add Todo" />
+  </form>
+
+  {{ input_content }}
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const todos = ref([]);
+const name = ref("");
+
+const addTodo = () => {
+  if (input_content.value.trim() === "") {
+    return;
   }
-}
+
+  todos.value.push({
+    title: input_content.value,
+    completed: false,
+    createdAt: new Date().getTime(),
+  });
+
+  watch(
+    todos,
+    (newTodo) => {
+      localStorage.setItem("todos", JSON.stringify(newTodo));
+    },
+    {
+      deep: true,
+    }
+  );
+};
+
+const input_content = ref("");
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || " ";
+});
 </script>
 
 <style>
